1f8efa1ec21528de4e3c906b2be569aeb00d02b5
Dylan Chen <hjchen2005@gmail.com>
2015-11-06 01:04:29 -0500

    Create thread_incr.h

---
+++ thread_incr.h
@@ -0,0 +1,37 @@
+#ifndef THREAD_INCR.H
+#define THREAD_INCR.H
+
+struct arguments {
+    int loops;
+    pthread_t self;
+};
+
+static void * threadFunc(void *arg);
+void printPt(pthread_t pt);
+static volatile int glob = 0;   /* "volatile" prevents compiler optimizations
+                                   of arithmetic operations on 'glob' */
+static void * threadFunc(void *arg){
+    struct arguments * threadFuncArgs = arg;
+    int threadFuncLoops = threadFuncArgs->loops;
+	threadFuncArgs->self = pthread_self();
+	printPt(threadFuncArgs->self);
+    int loc, j;
+	printf("\n glob = %d\n", glob);
+    for (j = 0; j < threadFuncLoops; j++) {
+        loc = glob;
+        loc++;
+        glob = loc;
+    }
+	printf("\n now glob = %d\n", glob);
+    return NULL;
+}
+
+void printPt(pthread_t pt) {
+  unsigned char *ptc = (unsigned char*)(void*)(&pt);
+  //printf(f, "0x");
+  for (size_t i=0; i<sizeof(pt); i++) {
+    printf("%02x", (unsigned)(ptc[i]));
+  }
+}
+
+#endif
