75ab3d784e54a01c77980120e00b724126a3e176
Dylan Chen <hjchen2005@gmail.com>
2015-11-06 01:04:07 -0500

    Create thread_incr.c

---
+++ thread_incr.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include "tlpi_hdr.h"
+#include "thread_incr.h"
+
+
+int main(int argc, char *argv[]) {
+    pthread_t t1, t2;
+    int loops, s;
+	struct arguments newArgument1;
+	struct arguments newArgument2;
+
+    newArgument1.loops = newArgument2.loops (argc > 1) ? getInt(argv[1], GN_GT_0, "num-loops") : 10000000;
+
+    s = pthread_create(&t1, NULL, threadFunc, &newArgument1);
+    if (s != 0)
+        errExitEN(s, "pthread_create");
+    s = pthread_create(&t2, NULL, threadFunc, &newArgument2);
+    if (s != 0)
+        errExitEN(s, "pthread_create");
+
+    s = pthread_join(t1, NULL);
+    if (s != 0)
+        errExitEN(s, "pthread_join");
+    s = pthread_join(t2, NULL);
+    if (s != 0)
+        errExitEN(s, "pthread_join");
+
+    //printf("glob = %d\n", glob);
+    
+    exit(EXIT_SUCCESS);
+}
